3abcfe3937e015dd9632742824b1535e9ef85b5b
patrick.gazaneo <patrick.gazaneo@stoneage.com.br>
2016-01-15 19:03:59 -0200

    First Blood

---
+++ Dojo.Tests/NumberParserTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dojo.Tests
+{
+    [TestClass]
+    public class NumberParserTest
+    {
+        [TestMethod]
+        public void test_number_1_to_roman_I()
+        {
+            string expected = "I";
+            string result = NumberParser.ParseToRoman(1);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_number_2_to_roman_II()
+        {
+            string expected = "II";
+            string result = NumberParser.ParseToRoman(2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_number_3_to_roman_III()
+        {
+            string expected = "III";
+            string result = NumberParser.ParseToRoman(3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_number_4_to_roman_IV()
+        {
+            string expected = "IV";
+            string result = NumberParser.ParseToRoman(4);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Dojo/NumberParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo
+{
+    public class NumberParser
+    {
+        public static string ParseToRoman(int number)
+        {
+            Dictionary<int, string> Mapper = new Dictionary<int, string> {
+                {1, "I"},
+                {5, "V"}
+            };
+            string result = "";
+            for (int i = 0; i < number; i++)
+            {
+                result += "I";
+            }
+            return result;
+        }
+    }
+}

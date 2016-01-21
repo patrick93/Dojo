e62d321aa4b44deebab2984a557600d4aeecbf1e
patrick.gazaneo <patrick.gazaneo@stoneage.com.br>
2016-01-21 15:05:16 -0200

    Template inicial dojo

--- Dojo.Tests/NumberParserTest.cs
+++ Dojo.Tests/NumberParserTest.cs
@@ -7,66 +7,9 @@ namespace Dojo.Tests
     public class NumberParserTest
     {
         [TestMethod]
-        public void test_number_1_to_roman_I()
+        public void Test_Method()
         {
-            string expected = "I";
-            string result = NumberParser.ParseToRoman(1);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_2_to_roman_II()
-        {
-            string expected = "II";
-            string result = NumberParser.ParseToRoman(2);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_3_to_roman_III()
-        {
-            string expected = "III";
-            string result = NumberParser.ParseToRoman(3);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_4_to_roman_IV()
-        {
-            string expected = "IV";
-            string result = NumberParser.ParseToRoman(4);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_5_to_roman_V()
-        {
-            string expected = "V";
-            string result = NumberParser.ParseToRoman(5);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_6_to_roman_VI()
-        {
-            string expected = "VI";
-            string result = NumberParser.ParseToRoman(6);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void test_number_9_to_roman_IX()
-        {
-            string expected = "IX";
-            string result = NumberParser.ParseToRoman(9);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void test_number_0_to_throw_exception()
-        {
-            string result = NumberParser.ParseToRoman(0);
+            
         }
     }
 }
--- Dojo/NumberParser.cs
+++ Dojo/NumberParser.cs
@@ -8,36 +8,5 @@ namespace Dojo
 {
     public class NumberParser
     {
-        private static List<RomanNumber> Mapper = new List<RomanNumber>(){
-            new RomanNumber(1, "I"),
-            new RomanNumber(4, "IV"),
-            new RomanNumber(5, "V"),
-            new RomanNumber(9, "IX")
-        };
-
-        public static string ParseToRoman(int number)
-        {
-            if (number < 1)
-            {
-                throw new ArgumentException();
-            }
-
-            string result = "";
-            RomanNumber closest = null;
-            while (number > 0)
-            {
-                foreach (RomanNumber item in Mapper)
-                {
-                    if (item.Number > number)
-                    {
-                        break;
-                    }
-                    closest = item;
-                }
-                result += closest.Roman;
-                number -= closest.Number;
-            }
-            return result;
-        }
     }
 }
